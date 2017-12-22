7e3838d8926e3fd535751374cd0db474ca86008a
DimitarTerzov <dimiterat@gmail.com>
2017-12-22 11:45:12 +0200

    Franch class initial implementation.

--- tdd_by_example/test_multi_currency.py
+++ tdd_by_example/test_multi_currency.py
@@ -3,11 +3,42 @@ class Dollar(object):
     def __init__(self, amount):
         self.amount = amount
 
+    def __eq__(self, other):
+        return self.amount == other.amount
+
     def times(self, multiplier):
-        self.amount *= multiplier
+        return Dollar(self.amount * multiplier)
+
+    def equals(self, other):
+        return self.amount == other.amount
+
+
+class Franc(object):
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __eq__(self, other):
+        return self.amount == other.amount
+
+    def times(self, multiplier):
+        return Franc(self.amount * multiplier)
+
+    def equals(self, other):
+        return self.amount == other.amount
 
 
 def test_multiplication():
     five = Dollar(5)
-    five.times(2)
-    assert five.amount == 10
+    assert Dollar(10) == five.times(2)
+    assert Dollar(15) == five.times(3)
+
+
+def test_equality():
+    assert Dollar(5).equals(Dollar(5))
+    assert not Dollar(5).equals(Dollar(6))
+
+
+def test_franc_multiplication():
+    five = Franc(5)
+    assert Franc(10) == five.times(2)
+    assert Franc(15) == five.times(3)
