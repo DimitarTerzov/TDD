ae638929f4500ebda4c9f88bf8b34cad546f75a6
DimitarTerzov <dimiterat@gmail.com>
2018-03-02 00:57:28 +0200

    Begin implementation of addition.

--- tdd_by_example/test_multi_currency.py
+++ tdd_by_example/test_multi_currency.py
@@ -4,11 +4,10 @@ class Money(object):
         self._currency = currency
 
     def __eq__(self, other):
-        return self._amount == other._amount
-
-    def equals(self, other):
         return (self._amount == other._amount and
                 self._currency == other._currency)
+
+    equals = __eq__
 
     @staticmethod
     def dollar(amount):
@@ -19,10 +18,18 @@ class Money(object):
         return Money(amount, "CHF")
 
     def times(self, multiplier):
-        return Money(self._amount*multiplier, self._currency)
+        return Money(self._amount * multiplier, self._currency)
 
     def currency(self):
         return self._currency
+
+    def plus(self, addend):
+        return Money(self._amount + addend._amount, self._currency)
+
+
+class Bank():
+    def reduse(self, source, to_currency):
+        return Money.dollar(10)
 
 
 def test_currency():
@@ -40,3 +47,11 @@ def test_equality():
     assert Money.dollar(5).equals(Money.dollar(5))
     assert not Money.dollar(5).equals(Money.dollar(6))
     assert not Money.franc(5).equals(Money.dollar(5))
+
+
+def test_simple_addition():
+    five = Money.dollar(5)
+    sum_ = five.plus(five)
+    bank = Bank()
+    reduced = bank.reduse(sum_, "USD")
+    assert Money.dollar(10) == reduced
